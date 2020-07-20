376e0016e83a4a5e76e553ceb3b917a7d55ef50d
Raphaël Thériault <raphael_theriault@outlook.com>
2020-07-20 14:49:59 -0400

    Add a couple new fields

--- schema.sql
+++ schema.sql
@@ -1,14 +1,18 @@
 CREATE TABLE users (
-    id           VARCHAR(255) NOT NULL PRIMARY KEY,
-    sign_up_at   DATETIME     NOT NULL,
-    sign_in_at   DATETIME,
+    id           VARCHAR(64)  NOT NULL PRIMARY KEY,
+    inserted_at  DATETIME     NOT NULL,
+    updated_at   DATETIME     NOT NULL,
+    login_at     DATETIME     NOT NULL,
 
-    password     VARCHAR(255),
+    display_name VARCHAR(64),
+    about        TEXT,
 
-    google_id    VARCHAR(255),
-    microsoft_id VARCHAR(255),
-    facebook_id  VARCHAR(255),
-    twitter_id   VARCHAR(255),
-    github_id    VARCHAR(255),
-    discord_id   VARCHAR(255)
+    password     VARCHAR(256),
+
+    google_id    VARCHAR(256),
+    microsoft_id VARCHAR(256),
+    facebook_id  VARCHAR(256),
+    twitter_id   VARCHAR(256),
+    github_id    VARCHAR(256),
+    discord_id   VARCHAR(256)
 );
